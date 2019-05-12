fc799e6ecce13e80e1fe9afc1c62f0b66d05af1f
AntonioLimaSilva <luclimasilva@hotmail.com>
2019-05-12 18:11:41 -0300

    Add config actuator metrics

--- src/main/java/br/com/luciano/beerstore/config/SecurityConfig.java
+++ src/main/java/br/com/luciano/beerstore/config/SecurityConfig.java
@@ -51,9 +51,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().csrf().disable().
-                authorizeRequests()
+        http.httpBasic().and().csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/actuator/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
